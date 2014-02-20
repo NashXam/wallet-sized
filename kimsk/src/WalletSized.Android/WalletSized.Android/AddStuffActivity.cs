52a52714e869f3f4d52c938553b245cbbb5102ce
Karlkim Suwamongkol <karlkim@outlook.com>
2014-02-19 22:01:38 -0600

    1st prototype
    Use buttons to add note. Only 1st button works right now :-)

---
+++ kimsk/src/WalletSized.Android/WalletSized.Android/AddStuffActivity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace WalletSized.Android
+{
+	[Activity (Label = "AddStuffActivity")]			
+	public class AddStuffActivity : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			SetContentView (Resource.Layout.AddStuff);
+
+			var label = FindViewById<TextView> (Resource.Id.screen2Label);
+			label.Text = Intent.GetStringExtra("FirstData") ?? "Data not available";
+
+			Button addNote = FindViewById<Button> (Resource.Id.AddNote);
+
+			addNote.Click += delegate {
+
+				var myNote = FindViewById<EditText> (Resource.Id.MyNote).Text;
+
+				var main = new Intent(this, typeof(MainActivity));
+				main.PutExtra("FirstData", myNote);
+				StartActivity(main);
+			};
+		}
+	}
+}
+
---
+++ kimsk/src/WalletSized.Android/WalletSized.Android/MainActivity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace WalletSized.Android
+{
+	[Activity (Label = "WalletSized.Android", MainLauncher = true)]
+	public class MainActivity : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			Button one = FindViewById<Button> (Resource.Id.One);
+
+			if (!string.IsNullOrEmpty (Intent.GetStringExtra ("FirstData"))) {
+				one.Text = Intent.GetStringExtra ("FirstData");
+			}
+
+			one.Click += delegate {
+				var addStuff = new Intent(this, typeof(AddStuffActivity));
+				addStuff.PutExtra("FirstData", "Add Stuff to the 1st Slot");
+				StartActivity(addStuff);
+			};
+		}
+	}
+}
+
+
